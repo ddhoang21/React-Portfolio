43c72d3217f875b0c1a5ef19c3cfa348358dc593
DDH <ddhoang1993@gmail.com>
2020-04-17 15:07:34 -0700

    Updates

---
+++ precache-manifest.57d403335125d8257db646286db2478b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a570c66941abe2d9aae21f3770f337",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "36546c755ea492933af2",
+    "url": "/React-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fdf3c205cb3db5bf0a4f",
+    "url": "/React-Portfolio/static/js/2.5643803d.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/React-Portfolio/static/js/2.5643803d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36546c755ea492933af2",
+    "url": "/React-Portfolio/static/js/main.0dbc0e5b.chunk.js"
+  },
+  {
+    "revision": "1bdac82cd996ae1ecb8f",
+    "url": "/React-Portfolio/static/js/runtime-main.e19ca9a5.js"
+  }
+]);
